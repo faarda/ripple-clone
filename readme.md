25f6a918e9b41362640aab38c493b2e71262f6a7
Silas <silasfaarda@gmail.com>
2021-05-25 22:26:56 +0100

    initialized project, with tailwind, gulp & sass

--- readme.md
+++ readme.md
@@ -1,9 +1,4 @@
-# Gulp with TailwindCSS Starter Kit
-
-Gulp with TailwindCSS Starter Kit ( Updated with [TailwindCSS JIT](https://github.com/tailwindlabs/tailwindcss-jit) )- A repo which makes your development easier with predefined gulp tasks that help you to use [tailwindcss](https://github.com/tailwindcss/tailwindcss) with simple npm commands 
-
 ## Usage
-
 1. Install Dev Depedencies
 ```sh
 npm install // or yarn install
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   purge: ['./src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        sm: '0.25rem',
+      },
+      spacing: {
+        '7.5': '1.875rem',
+        15: '3.75rem',
+      },
+      colors: {
+        dark: '#171C21',
+        primary: {
+          300: '#0F72E5',
+          500: '#1266C8'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
